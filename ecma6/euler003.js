603093fb2ddac831f31b8566541ee97108db5b31
Greg Charles <gch@csatp.com>
2016-08-25 11:15:25 -0700

    Updated solution for Euler 3.

--- ecma6/euler003.js
+++ ecma6/euler003.js
@@ -7,19 +7,8 @@
 //  What is the largest prime factor of the number 600851475143 ?
 //
 function euler003() {
+   const number = 600851475143;
 
-
-   let number = 600851475143;
-   let gen = primes.primeGenerator();
-   let possibleFactors = [];
-
-   for (let prime = gen.next().value; prime * prime <= number; prime = gen.next().value) {
-      possibleFactors.push(prime);
-   }
-
-   for (let i = possibleFactors.length -1; i >= 0; i--) {
-      if (number % possibleFactors[i] == 0) {
-         return possibleFactors[i];
-      }
-   }
+   let factorMap = primes.primeFactors(number);
+   return Math.max.apply(null, [...factorMap.factors()]);
 }
