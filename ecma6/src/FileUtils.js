91fc088807c351501a79329023599bd9f8647c60
Greg Charles <gch@csatp.com>
2016-10-14 19:55:05 -0700

    Commit all local updates.

---
+++ ecma6/src/FileUtils.js
@@ -0,0 +1,21 @@
+/**
+ * Created by gcharles on 10/11/16.
+ */
+function readEulerDataFile(fileName) {
+    let allText = null;
+    let rawFile = new XMLHttpRequest();
+
+    rawFile.open("GET", `http://localhost:63343/ecma6/data/${fileName}`, false);
+    //rawFile.open("GET", `http://http://projecteuler.net/project/resources//${fileName}`, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
+            }
+        }
+    };
+    rawFile.send(null);
+    return allText;
+}
