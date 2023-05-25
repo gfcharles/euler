9f66f48463169c1b12965b30782f90943a3578d9
Greg Charles <gregcharles@javaranch.com>
2023-05-24 18:40:40 -0700

    Updated 36

--- python/euler036.py
+++ python/euler036.py
@@ -1,24 +1,42 @@
-'''
+"""
 The decimal number, 585 = 1001001001_(2) (binary), is palindromic in both bases.
 
 Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
 (Please note that the palindromic number, in either base, may not include leading zeros.)
+"""
+import logging
+import euler
 
-Created on Oct 30, 2010
 
-@author: Greg Charles
-'''
-def is_palindrome(s):
-    return s == s[::-1]
+@euler.euler_problem
+def euler036(n:int|str) -> int:
+    return sum(x for x in range(1, int(n)) if test_palindrome(x))
+
+
+def test_palindrome(n: int) -> bool:
+    decimal = str(n)
+    binary = binstr(n)
+
+    if is_palindrome(decimal) and is_palindrome(binary):
+        logging.debug(f"Found: {decimal} ({binary})")
+        return True
+    return False
+
+def is_palindrome(s:str) -> bool:
+    half = len(s) // 2
+    return  s[:half] == s[-1: -(half + 1): -1]
 
 # Convert a positive integer to a binary string. 
 # Will return an empty string for 0 or negative
-def binstr(n):
+def binstr(n:int) -> str:
     string = ""
     while n > 0:
-        string = ("1" if n & 1 else "0") + string
+        string += str(n & 1)
         n = n >> 1 
-    return string
+    return string[::-1]
 
-print sum(n for n in xrange(1,1000000) if (is_palindrome(str(n)) and is_palindrome(binstr(n))))
+
+if __name__ == '__main__':
+    euler.config_log_level(logging.DEBUG)
+    print(euler036(1_000_000))
---
+++ python/tests/test_euler036.py
@@ -0,0 +1,6 @@
+from tests.test_euler import TestEuler, TestEulerSetup
+from euler036 import euler036
+
+class TestEuler036(TestEulerSetup, TestEuler):
+    problem_number = 36
+    function = euler036
